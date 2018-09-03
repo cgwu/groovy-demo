496cf55181db4124cfc1b809470714c94c28fcfa
cgwu <gg.wcg@qq.com>
2018-09-03 13:12:00 +0800

    init

---
+++ src/main/java/Foo.groovy
@@ -0,0 +1,12 @@
+/**
+ * Created by sam on 18-9-3.
+ */
+class Foo {
+    static void main(args){
+        println 'hello中国'
+        println()
+        def str='世界'
+        def i = 123
+        println("str: $str, i: $i, date: ${new Date()}")    //GString
+    }
+}
